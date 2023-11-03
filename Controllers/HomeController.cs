a6187a8b1dc212151cb0b22ea2effd12a1802c55
Cayuyo <cayuken00@gmail.com>
2023-11-03 05:54:09 -0300

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Encuesta_Dojo.Models;
+
+namespace Encuesta_Dojo.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    [HttpPost]
+    [Route("result")]
+    public IActionResult Result(string Name, string Location, string Language, string Comment)
+    {
+        ViewBag.Name = Name;
+        ViewBag.Location = Location;
+        ViewBag.Language = Language;
+        ViewBag.Comment = Comment;
+        return View("Result");
+
+    }
+
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Encuesta_Dojo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
